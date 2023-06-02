64cc1d5bad1642228ebe7b03730379d79f84f5c7
sivaprasadram <sivaprasadv9323@gmail.com>
2023-06-02 11:13:36 +0530

    Add project files.

---
+++ Employee_CRUD_MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Employee_CRUD_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee_CRUD_MVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+
+}
+
